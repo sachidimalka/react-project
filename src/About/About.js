95a764f461ded72d35b3e155812f5e5218d66fb8
Sachini Siriwardhana <48342573+sachidimalka@users.noreply.github.com>
2023-02-08 16:23:52 +0530

    Feat(AboutPage):Create about page

---
+++ src/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>My Page</h1>
+      <p>This is my new page</p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import AboutPage from './About/AboutPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about-page">My Page</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about-page" component={AboutPage} />
+      </div>
+    </Router>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the home page</p>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
